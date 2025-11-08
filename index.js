44ac7de19a2dd4fc3eea736789c20d82b5c0e2e2
Shuhrat <shuhakomilov1012@gmail.com>
2025-11-08 22:09:58 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const drums = document.querySelectorAll('.drum')
+
+function onClick() {
+    playSound(this.innerHTML)
+    playAnimation(this)
+}
+
+for (i = 0; i < drums.length; i++) {
+    drums[i].addEventListener('click', onClick)
+}
+
+document.addEventListener('keydown', onKeyDown)
+
+function onKeyDown(event) {
+    playSound(event.key)
+    const activeButton = document.querySelector('.' + event.key)
+    playAnimation(activeButton)
+}
+
+function playSound(letter) {
+    switch (letter) {
+        case 'w':
+            var crash = new Audio('./sounds/crash.mp3');
+            crash.play();
+            break
+        case 'a':
+            var kick = new Audio('./sounds/kick-bass.mp3')
+            kick.play()
+            break
+        case 's':
+            var snare = new Audio('./sounds/snare.mp3')
+            snare.play()
+            break
+        case 'd':
+            var tom1 = new Audio('./sounds/tom-1.mp3')
+            tom1.play()
+            break
+        case 'j':
+            var tom2 = new Audio('./sounds/tom-2.mp3')
+            tom2.play()
+            break
+        case 'k':
+            var tom3 = new Audio('./sounds/tom-3.mp3')
+            tom3.play()
+            break
+        case 'l':
+            var tom4 = new Audio ('./sounds/tom-4.mp3')
+            tom4.play()
+        default: console.log(this.innerHTML)
+    }
+}
+
+function playAnimation(activeButton){
+    activeButton.classList.add('pressed')
+    setTimeout(function () {
+        activeButton.classList.remove('pressed')
+    }, 100)
+}
